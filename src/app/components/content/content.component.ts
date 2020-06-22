3fdb84e5c9bc6f54a0218bce130748b30c16c202
ign3el <sahtesham@gmail.com>
2020-06-23 03:27:15 +0400

    saas

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -8,13 +8,30 @@ import { FirebaseService } from 'src/app/services/firebase.service';
 })
 export class ContentComponent implements OnInit {
 
+  btnTxt :string = "Start Bot";
+  looping: boolean = false;
+  textArea: string = "=======SniperBot=======";
+  i: number = 0;
+
   constructor(private firebase:FirebaseService) { }
 
   ngOnInit(): void {
   }
 
   onSubmit() {
-    this.firebase.addData();
+    if (this.btnTxt == "Start Bot") {
+      this.btnTxt = "Stop Bot";
+      this.looping = true;
+    } else {
+      this.btnTxt = "Start Bot";
+      this.looping = false;
+    }
+
+    while (this.looping) {
+      this.i++;
+      this.textArea += "\nLooping "+this.i+" times";
+    }
+
   }
 
 }
